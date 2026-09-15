2856332cdc55fb67284cb482cafe952b0b638e9e
Matryoshika <mad.matryoshika@gmail.com>
2026-09-15 09:29:05 +0200

    Remove unneeded math

--- custom_bag/server_scripts/custom_bag.js
+++ custom_bag/server_scripts/custom_bag.js
@@ -12,10 +12,9 @@ ItemEvents.rightClicked('minecraft:stick', event => {
 	
 	//Create an empty representation of the bag, we will fill this later
 	let EMPTY_GUI = [];
-	let size = (total - EMPTY_GUI.length)
-	for(let i = 0; i < size; i++)
+	for(let i = 0; i < total; i++)
 		EMPTY_GUI.push(Item.of('minecraft:air'))
-
+		
 	let container = new JavaAdapter($SC, {
 		stillValid: function(player){
 			//ensure that we can always open this bag; it is not tied to a location
@@ -29,6 +28,7 @@ ItemEvents.rightClicked('minecraft:stick', event => {
 			//Ensures that we cannot put any bag inside a bag; stops us from putting the bag into itself, thus voiding everything
 			return stack.item.id != this.bagItem.item.id;
 		}
+
 	}, EMPTY_GUI)
 	
 	//VERY important! We must tie the actual held itemstack to the GUI, or we would not be able to read/write between them
@@ -36,8 +36,7 @@ ItemEvents.rightClicked('minecraft:stick', event => {
 	
 	//retrieve the contents from the held item and put them into the GUI
 	let contents = item.getOrDefault($DC.CONTAINER, $ICC.EMPTY)
-	contents.copyInto (container.getItems())
-	
+	contents.copyInto(container.getItems())
 	
 	player.openInventoryGUI(container, Component.translatable('container.inventory'), col, row)
 })
